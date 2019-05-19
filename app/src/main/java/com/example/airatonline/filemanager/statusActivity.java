7a71f963073dd8feec331e88a9bfcbba01b67bb4
Air <error.404.not.find@yandex.ru>
2019-05-19 18:49:38 +0300

    added gridview

--- app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
+++ app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
@@ -2,12 +2,21 @@ package com.example.airatonline.filemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class statusActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        gridView = findViewById(R.id.gridView);
+        String[] data = {"a", "b", "C", "d", "so many characters"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.statusitem, R.id.statusText, data);
+        gridView.setAdapter(adapter);
+
     }
 }
