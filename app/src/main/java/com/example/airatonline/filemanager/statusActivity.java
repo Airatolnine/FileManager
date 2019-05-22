b4ab4f79e7dbc72238e79acbb68eff13873c43a5
Air <error.404.not.find@yandex.ru>
2019-05-22 17:17:13 +0300

    rewrite countFun for avoid duplication, when counting

--- app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
+++ app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
@@ -4,6 +4,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -13,31 +14,46 @@ import java.io.File;
 public class statusActivity extends AppCompatActivity {
 
     GridView gridView;
+    int imgCountInt = 0;
+    int videoCountInt = 0;
+    int musicCountInt = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
         TextView imgCount = findViewById(R.id.imgCount);
-        String path = Environment.getExternalStorageDirectory() + "/DCIM/";
+        TextView videoView = findViewById(R.id.videoCount);
+        String path = Environment.getExternalStorageDirectory() + "/";
         Log.d("AAA", path);
-        Log.d("AAA", String.valueOf(imgCountFun(path)));
-        imgCount.setText(String.valueOf(imgCountFun(path)));
+        countFun(path);
+        imgCount.setText(String.valueOf(imgCountInt));
+        videoView.setText(String.valueOf(videoCountInt));
 
 
     }
 
-    int imgCountFun(String path) {
-        int res=0;
+
+    void countFun(String path) {
         File[] files = new File(path).listFiles();//Получили все файлы в DCIM
-        for(int i=0;i<files.length;i++){
-            if(files[i].isDirectory()){
-                res+=imgCountFun(path+"/"+files[i].getName());
-            }
-            else{
-                res++;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                countFun(path + "/" + files[i].getName());
+            } else {
+                String extension = files[i].getName().substring(files[i].getName().lastIndexOf('.') + 1);
+                //Log.d("AAA", extension);
+                String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
+                if (mimeType != null) {
+                    String mimeElement = mimeType.split("/")[0];
+                    if (mimeElement.equals("image")) {
+                        imgCountInt++;
+                    }
+                    else if(mimeElement.equals("video")){
+                        videoCountInt++;
+                    }
+                }
             }
         }
-        return res;
     }
 }
