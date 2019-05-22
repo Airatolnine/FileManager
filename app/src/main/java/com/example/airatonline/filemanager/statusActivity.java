8aaa59ee6e230aa67fea366bc941797d6b8e3471
Air <error.404.not.find@yandex.ru>
2019-05-22 15:26:50 +0300

    added image count and drawable for this in statusActivity

--- app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
+++ app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
@@ -1,9 +1,14 @@
 package com.example.airatonline.filemanager;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class statusActivity extends AppCompatActivity {
 
@@ -13,10 +18,26 @@ public class statusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-        gridView = findViewById(R.id.gridView);
-        String[] data = {"a", "b", "C", "d", "so many characters"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.statusitem, R.id.statusText, data);
-        gridView.setAdapter(adapter);
+        TextView imgCount = findViewById(R.id.imgCount);
+        String path = Environment.getExternalStorageDirectory() + "/DCIM/";
+        Log.d("AAA", path);
+        Log.d("AAA", String.valueOf(imgCountFun(path)));
+        imgCount.setText(String.valueOf(imgCountFun(path)));
 
+
+    }
+
+    int imgCountFun(String path) {
+        int res=0;
+        File[] files = new File(path).listFiles();//Получили все файлы в DCIM
+        for(int i=0;i<files.length;i++){
+            if(files[i].isDirectory()){
+                res+=imgCountFun(path+"/"+files[i].getName());
+            }
+            else{
+                res++;
+            }
+        }
+        return res;
     }
 }
