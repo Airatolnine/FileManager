b72b36811d6325ec3d2ed73b2eef6695eb724fef
Air <error.404.not.find@yandex.ru>
2019-05-23 12:02:53 +0300

    added text, archive, download labels

--- app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
+++ app/src/main/java/com/example/airatonline/filemanager/statusActivity.java
@@ -3,28 +3,28 @@ package com.example.airatonline.filemanager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.File;
 
 public class statusActivity extends AppCompatActivity {
 
-    GridView gridView;
+
     int imgCountInt = 0;
     int videoCountInt = 0;
     int musicCountInt = 0;
-    int applicationCountInt = 0;
+    int textCountInt = 0;
+    int archiveCountInt = 0;
+    int downloadCountInt = 0;
 
     Context context = this;
-
 
 
     @Override
@@ -33,14 +33,47 @@ public class statusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_status);
         TextView imgCount = findViewById(R.id.imgCount);
         TextView videoView = findViewById(R.id.videoCount);
-        ( findViewById(R.id.imgTap)).setOnClickListener(new View.OnClickListener() {
+        TextView musicView = findViewById(R.id.musicCount);
+        TextView textView = findViewById(R.id.textCount);
+        TextView arciveView = findViewById(R.id.archiveCount);
+        TextView downloadView = findViewById(R.id.downloadCount);
+        setDownloadCountInt();
+        (findViewById(R.id.imgTap)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MainActivity.class);
                 context.startActivity(intent);
             }
         });
-        ( findViewById(R.id.videoTap)).setOnClickListener(new View.OnClickListener() {
+        (findViewById(R.id.videoTap)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        (findViewById(R.id.musicTap)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        (findViewById(R.id.textTap)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        (findViewById(R.id.archiveTap)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        (findViewById(R.id.downloadTap)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MainActivity.class);
@@ -52,6 +85,10 @@ public class statusActivity extends AppCompatActivity {
         countFun(path);
         imgCount.setText(String.valueOf(imgCountInt));
         videoView.setText(String.valueOf(videoCountInt));
+        musicView.setText(String.valueOf(musicCountInt));
+        textView.setText(String.valueOf(textCountInt));
+        arciveView.setText(String.valueOf(archiveCountInt));
+        downloadView.setText(String.valueOf(downloadCountInt));
 
 
     }
@@ -59,29 +96,41 @@ public class statusActivity extends AppCompatActivity {
 
     void countFun(String path) {
         File[] files = new File(path).listFiles();//Получили все файлы в DCIM
-        for (int i = 0; i < files.length; i++) {
-            if (files[i].isDirectory()&& files[i].getName().charAt(0) != '.' && !files[i].getName().equals("Android")) {
-                countFun(path + "/" + files[i].getName());
+        for (File file : files) {
+            if (file.isDirectory() && file.getName().charAt(0) != '.' && !file.getName().equals("Android")) {
+                countFun(path + "/" + file.getName());
             } else {
-                String extension = files[i].getName().substring(files[i].getName().lastIndexOf('.') + 1);
+                String extension = file.getName().substring(file.getName().lastIndexOf('.') + 1);
+                if(extension.equals("rar")|| extension.equals("zip")){
+                    archiveCountInt++;
+                }
+
+
+
                 //Log.d("AAA", extension);
                 String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
                 if (mimeType != null) {
                     String mimeElement = mimeType.split("/")[0];
-                    if (mimeElement.equals("image")) {
-                        imgCountInt++;
-                    }
-                    else if(mimeElement.equals("video")){
-                        videoCountInt++;
-                    }
-                    else if(mimeElement.equals("audio")){
-                        musicCountInt++;
-                    }
-                    else if(mimeElement.equals("text") || mimeElement.equals("application")){
-                       applicationCountInt++;
+                    switch (mimeElement) {
+                        case "image":
+                            imgCountInt++;
+                            break;
+                        case "video":
+                            videoCountInt++;
+                            break;
+                        case "audio":
+                            musicCountInt++;
+                            break;
+                        case "text":
+                            textCountInt++;
+                            break;
                     }
                 }
             }
         }
+    }
+    void setDownloadCountInt(){
+        String path = Environment.getExternalStorageDirectory() + "/Download/";
+        downloadCountInt = (new File(path).listFiles().length);
     }
 }
